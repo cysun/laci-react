15a1d6457deda4de1ed6f36ddbaf0465106cb4e7
Chengyu Sun <csun@calstatela.edu>
2021-09-02 10:13:43 -0700

    CityDashboard formatting

--- src/components/city-dashboard.tsx
+++ src/components/city-dashboard.tsx
@@ -1,4 +1,5 @@
-import { Descriptions } from "antd";
+import { history } from "umi";
+import { Row, Col, Card, Button, Tag, Statistic } from "antd";
 import { PageContainer } from "@ant-design/pro-layout";
 import { Line, LineConfig } from "@ant-design/charts";
 
@@ -25,32 +26,72 @@ export interface ChartDataEntry {
 }
 
 export default function CityDashboard({ cityInfo }: { cityInfo: CityInfo }) {
-  let lineConfig: LineConfig = {
-    data: cityInfo.chartData,
+  let cases = cityInfo.chartData?.flatMap((d) => [
+    { label: d.label, value: d.cases, type: "Cases" },
+    { label: d.label, value: d.cases7Avg, type: "Cases (7-day Average)" },
+  ]);
+  let deaths = cityInfo.chartData?.flatMap((d) => [
+    { label: d.label, value: d.deaths, type: "Deaths" },
+    { label: d.label, value: d.deaths7Avg, type: "Deaths (7-day Average)" },
+  ]);
+
+  let config: LineConfig = {
     xField: "label",
-    yField: "cases7Avg",
+    yField: "value",
+    seriesField: "type",
+    color: ["yellow", "blue"],
   };
 
   return (
-    <PageContainer header={{ title: cityInfo.name }}>
-      <Descriptions layout="vertical" bordered>
-        <Descriptions.Item label="Last Updated">
-          {cityInfo.lastUpdated?.toLocaleDateString()}
-        </Descriptions.Item>
-        <Descriptions.Item label="Population">
-          {cityInfo.population?.toLocaleString()}
-        </Descriptions.Item>
-        <Descriptions.Item label="Test">
-          {cityInfo.tests?.toLocaleString()}
-        </Descriptions.Item>
-        <Descriptions.Item label="Cases">
-          {cityInfo.cases?.toLocaleString()}
-        </Descriptions.Item>
-        <Descriptions.Item label="Deaths">
-          {cityInfo.deaths?.toLocaleString()}
-        </Descriptions.Item>
-      </Descriptions>
-      <Line {...lineConfig} />
+    <PageContainer
+      header={{ title: cityInfo.name }}
+      tags={[
+        <Tag color="green">{cityInfo.lastUpdated?.toLocaleDateString()}</Tag>,
+      ]}
+      extra={[
+        <Button onClick={() => history.push("/")}>Back to Cities</Button>,
+      ]}
+    >
+      <Row gutter={[16, 16]}>
+        <Col xs={12} sm={6}>
+          <Card>
+            <Statistic title="Population" value={cityInfo.population} />
+          </Card>
+        </Col>
+        <Col xs={12} sm={6}>
+          <Card>
+            <Statistic title="Tests" value={cityInfo.tests} />
+          </Card>
+        </Col>
+        <Col xs={12} sm={6}>
+          <Card>
+            <Statistic
+              title="Cases"
+              value={cityInfo.cases}
+              valueStyle={{ color: "orange" }}
+            />
+          </Card>
+        </Col>
+        <Col xs={12} sm={6}>
+          <Card>
+            <Statistic
+              title="Deaths"
+              value={cityInfo.deaths}
+              valueStyle={{ color: "red" }}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Card title="New Cases">
+            <Line data={cases} {...config} />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Card title="New Deaths">
+            <Line data={deaths} {...config} />
+          </Card>
+        </Col>
+      </Row>
     </PageContainer>
   );
 }
